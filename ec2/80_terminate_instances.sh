aa0e43c5201c3b7e87e021ff7657574196aae101
Rasp50@example.com <Rasp50@example.com>
2020-12-15 21:29:41 +0530

    a

--- ec2/80_terminate_instances.sh
+++ ec2/80_terminate_instances.sh
@@ -1,4 +1,4 @@
 #Terminate all instances
-aws ec2 --profile shibu_admin terminate-instances --instance-ids $(aws ec2 describe-instances --filters  "Name=instance-state-name,Values=pending,running,stopped,stopping" --query "Reservations[].Instances[].[InstanceId]" --output text | tr '\n' ' ')
+aws ec2 --profile shibu_admin terminate-instances --instance-ids $(aws ec2 --profile shibu_admin describe-instances --filters  "Name=instance-state-name,Values=pending,running,stopped,stopping" --query "Reservations[].Instances[].[InstanceId]" --output text | tr '\n' ' ')
 rm ec2-hosts
 
